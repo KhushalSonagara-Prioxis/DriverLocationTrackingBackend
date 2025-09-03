033a7e5ee841532c24a7b8ecdacc084784d27527
Kishanprioxis <Kishan@prioxis.com>
2025-09-03 11:40:10 +0530

    cors resolved

--- DLT.Api/Program.cs
+++ DLT.Api/Program.cs
@@ -16,94 +16,89 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        
-        // Add services to the container.
+
         builder.Services.AddScoped<IDriverRepository, DriverRepository>();
         builder.Services.AddScoped<ITripRepository, TripRepository>();
-        
-        //connection String
-        // Get connection string from appsettings.json
+
+        // Connection String
         var connectionString = builder.Configuration.GetConnectionString("DBConnection");
-                //serilogger
+
+        // Serilog
         Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
-            .WriteTo.File("log.txt",
-                rollingInterval: RollingInterval.Day,
-                rollOnFileSizeLimit: true)
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
             .CreateLogger();
-        //Fluent Validation
+
+        // FluentValidation
         builder.Services.AddControllers()
-        
             .AddFluentValidation(fv =>
-        
             {
-        
                 fv.RegisterValidatorsFromAssembly(AppDomain.CurrentDomain.GetAssemblies()
-        
                     .SingleOrDefault(assembly => assembly.GetName().Name == typeof(Program).Assembly.GetName().Name));
-        
             });
+
         builder.Services.AddDbContext<DriverLocationTrackingDbContext>(options =>
         {
-            options.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOptions =>
-            {
-        
-            });
+            options.UseSqlServer(connectionString);
             options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             options.EnableSensitiveDataLogging(true);
         }, ServiceLifetime.Transient);
 
         builder.Services.AddDbContext<DriverLocationTrackingSpContext>(options =>
         {
-            options.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOptions =>
+            options.UseSqlServer(connectionString, sqlOptions =>
             {
-
                 sqlOptions.EnableRetryOnFailure();
-
             });
             options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             options.EnableSensitiveDataLogging(true);
         }, ServiceLifetime.Transient);
-        // Register the UserService
-        // builder.Services.AddCors(options =>
-        // {
-        //     options.AddPolicy("AllowAll", policy =>
-        //     {
-        //         policy.AllowAnyOrigin()
-        //             .AllowAnyMethod()
-        //             .AllowAnyHeader();
-        //     });
-        //
-        //     // OR restrict to specific origin (better for production)
-        //     options.AddPolicy("AllowFrontend", policy =>
-        //     {
-        //         policy.WithOrigins("http://localhost:3000") // frontend URL
-        //             .AllowAnyMethod()
-        //             .AllowAnyHeader();
-        //     });
-        // });
+
+        // ✅ Register CORS policy
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+            });
+
+            // (Better for production)
+            options.AddPolicy("AllowFrontend", policy =>
+            {
+                policy.WithOrigins(
+                    "http://localhost:3000",
+                    "http://192.168.1.119:3000"
+                )
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+        });
+
         UnitOfWorkServiceCollectionExtentions.AddUnitOfWork<DriverLocationTrackingDbContext>(builder.Services);
+
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
         app.UseMiddleware<ExceptionMiddleware>();
         app.UseHttpsRedirection();
+
+        // ✅ Enable CORS (use AllowAll for dev)
         app.UseCors("AllowAll");
+        // Or use specific allowed origins in production:
+        // app.UseCors("AllowFrontend");
+
         app.UseAuthorization();
-
-
         app.MapControllers();
-
         app.Run();
     }
 }
