bbc247c50bea671615e8284e4c69de01b2343565
Kishanprioxis <Kishan@prioxis.com>
2025-09-05 16:45:52 +0530

    change the SP of GetAll trips

--- DLT.Models/ResponsetModel/TripListResponseModel.cs
+++ DLT.Models/ResponsetModel/TripListResponseModel.cs
@@ -6,6 +6,10 @@ public class TripListResponseModel
     public string StartLocationName { get; set; }
     public string ToLocationName { get; set; }
     public string TripStatusName { get; set; }
+    public decimal StartLatitude { get; set; }
+    public decimal StartLongitude { get; set; }
+    public decimal ToLatitude { get; set; }
+    public decimal ToLongitude { get; set; }
     public int TripStatus { get; set; }
     public string DriverName { get; set; }
     public DateTime LastModifiedDate { get; set; }
